ce6d586e2b335bae6229832973b0920cfd9e18c7
Bruno Maia <brunomaiapeixoto@hotmail.com>
2023-09-18 14:10:02 -0300

    Desafios AWS e Santander

--- src/DesafioAWS/CustosAWS.java
+++ src/DesafioAWS/CustosAWS.java
@@ -1,2 +1,29 @@
-package DesafioAWS;public class CustosAWS {
+package DesafioAWS;
+
+import java.util.Scanner;
+
+public class CustosAWS {
+    public static void main(String[] args) {
+
+        double custoPorHoraServidor = 0.15;
+        double custoPorHoraBancoDados = 0.05;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("quantidade de servidores necessários: ");
+        int quantidadeServidores = scanner.nextInt();
+
+        System.out.println("quantidade de servidores necessários:   ");
+        int quantidadeBancosDados = scanner.nextInt();
+
+        double custoServidores = quantidadeServidores * custoPorHoraServidor;
+        double custoBancosDados = quantidadeBancosDados * custoPorHoraBancoDados;
+        double custoTotal = custoServidores + custoBancosDados;
+
+
+
+        System.out.printf("Custo total de uso da AWS por hora: R$ %.2f%n", custoTotal);
+
+        scanner.close();
+    }
 }
--- src/DesafioAWS/VerificandoSenhasAWS.java
+++ src/DesafioAWS/VerificandoSenhasAWS.java
@@ -1,4 +1,53 @@
 package DesafioAWS;
 
-public class ForçaSenha {
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class VerificandoSenhasAWS {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String senha = scanner.nextLine();
+
+        String resultado = verificarForcaSenha(senha);
+        System.out.println(resultado);
+    }
+
+    public static String verificarForcaSenha(String senha) {
+        int comprimentoMinimo = 8;
+
+        boolean temLetraMaiuscula = Pattern.compile("[A-Z]").matcher(senha).find();
+        boolean temLetraMinuscula = Pattern.compile("[a-z]").matcher(senha).find();
+        boolean temNumero = Pattern.compile("[0-9]").matcher(senha).find();
+        boolean temCaractereEspecial = Pattern.compile("[!@#$%¨&*()\\-+\\=<>.,./?|]").matcher(senha).find();
+        boolean temSequenciaComum = senha.matches(".*(?i)123456.*|.*(?i)abcdef.*");
+        boolean temPalavraComum = senha.equalsIgnoreCase("password") || senha.equalsIgnoreCase("123456") || senha.equalsIgnoreCase("qwerty");
+
+        if (senha.length() < comprimentoMinimo) {
+            return "Sua senha e muito curta. Recomenda-se no minimo 8 caracteres.";
+        }
+
+        // Check for at least one uppercase letter
+        if (!Pattern.compile("[A-Z]").matcher(senha).find()) {
+            return "Sua senha nao atende aos requisitos de seguranca.";
+        }
+
+        if (!Pattern.compile("[a-z]").matcher(senha).find()) {
+            return "Sua senha nao atende aos requisitos de seguranca.";
+        }
+
+        if (!Pattern.compile("[0-9]").matcher(senha).find()) {
+            return "Sua senha nao atende aos requisitos de seguranca.";
+        }
+
+        if (!Pattern.compile("[!@#$%¨&*()\\-+\\=<>.,./?|]").matcher(senha).find()) {
+            return "Sua senha nao atende aos requisitos de seguranca.";
+        }
+
+        return "Sua senha atende aos requisitos de seguranca. Parabens!";
+
+
+
+
+    }
 }
